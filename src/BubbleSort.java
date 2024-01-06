97475409fabade5a3d345701be7cbcc36a026553
Andrew Santhosh <andrewsanthosh264@gmail.com>
2024-01-07 01:55:45 +0530

    sort inplace

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -6,7 +6,7 @@ public class BubbleSort {
     // actually the complexity is  (N^2)-N/2
     // we don't care about insignificant factors, so it is N^2
 
-    static int[] bubbleSort(int[] arr) {
+    static void bubbleSort(int[] arr) {
         for (int i = 0; i < arr.length; i++)
             for (int j = 0; j < arr.length - 1 - i; j++)
                 // -1 cuz we don't want the last element, as we compare with a [j+1]
@@ -16,13 +16,12 @@ public class BubbleSort {
                     arr[j] = arr[j + 1];
                     arr[j + 1] = temp;
                 }
-
-        return arr;
     }
 
     public static void main(String[] args) {
         int[] arr = {4, 5, 7, 2, 3, 6, 8, 9, 0, 1};
         System.out.println("Unsorted Array: " + Arrays.toString(arr));
-        System.out.println("After Bubble Sort: " + Arrays.toString(bubbleSort(arr)));
+        bubbleSort(arr);
+        System.out.println("After Bubble Sort: " + Arrays.toString(arr));
     }
 }
