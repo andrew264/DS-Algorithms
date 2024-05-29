ce9be4489512ed953bef6c53df1ae0b02815661d
Andrew Santhosh <andrewsanthosh264@gmail.com>
2024-05-29 22:47:12 +0530

    aahahah

---
+++ src/problems/RemoveInvalidParentheses.java
@@ -0,0 +1,67 @@
+package problems;
+
+import java.util.*;
+
+public class RemoveInvalidParentheses {
+
+    public static void main(String[] args) {
+        String input1 = "()())()";
+        String input2 = "(a)())()";
+        String input3 = ")(";
+        String input4 = "((abc)((de))";
+
+        System.out.println(removeInvalidParentheses(input4));
+        System.out.println(removeInvalidParentheses(input1));
+        System.out.println(removeInvalidParentheses(input2));
+        System.out.println(removeInvalidParentheses(input3));
+    }
+
+    public static List<String> removeInvalidParentheses(String s) {
+        List<String> result = new ArrayList<>();
+        if (s == null) return result;
+
+        Set<String> visited = new HashSet<>();
+        Queue<String> queue = new LinkedList<>();
+
+        queue.add(s);
+        visited.add(s);
+
+        boolean found = false;
+
+        while (!queue.isEmpty()) {
+            String current = queue.poll();
+
+            if (isValid(current)) {
+                result.add(current);
+                found = true;
+            }
+
+            if (found) continue;
+
+            for (int i = 0; i < current.length(); i++) {
+                if (current.charAt(i) != '(' && current.charAt(i) != ')') continue;
+
+                String next = current.substring(0, i) + current.substring(i + 1);
+
+                if (!visited.contains(next)) {
+                    queue.add(next);
+                    visited.add(next);
+                }
+            }
+        }
+
+        return result;
+    }
+
+    private static boolean isValid(String s) {
+        int count = 0;
+        for (char c : s.toCharArray()) {
+            if (c == '(') count++;
+            if (c == ')') count--;
+            if (count < 0) return false;
+        }
+        return count == 0;
+    }
+}
+
+
